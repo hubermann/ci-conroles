4e9b95d87fa7a3e769dced6d08d89b71107781ae
Hubermann <hubermann@gmail.com>
2017-10-23 20:30:40 -0300

    inicial| DB  - config -  url con htaccess fixed

---
+++ application/views/control/categoria_eventos/new_categoria_evento.php
@@ -0,0 +1,67 @@
+<?php  
+
+$attributes = array('class' => 'form-horizontal', 'id' => 'new_categoria_evento');
+echo form_open_multipart(base_url('control/categoria_eventos/create/'),$attributes);
+
+echo form_hidden('categoria_evento[id]');
+
+?>
+<legend><?php echo $title ?></legend>
+<div class="well well-large well-transparent">
+
+
+<!-- Text input-->
+<!--
+<div class="control-group">
+<label class="control-label">Categoria</label>
+	<div class="controls">
+		
+		<select name="categoria_id" id="categoria_id">
+		<?php  
+		/*
+		$categorias = $this->Categoria->get_records_menu();
+		if($categorias){
+
+			foreach ($categorias->result() as $value) {
+				echo '<option value="'.$value->id.'">'.$value->nombre.'</option>';
+			}
+		}
+		*/
+		?>
+		</select>
+
+		<?php echo form_error('categoria_id','<p class="error">', '</p>'); ?>
+	</div>
+</div>
+-->
+			<!-- Text input-->
+			<div class="control-group">
+			<label class="control-label">Nombre</label>
+			<div class="controls">
+			<input value="<?php echo set_value('nombre'); ?>" class="form-control" type="text" name="nombre" />
+			<?php echo form_error('nombre','<p class="error">', '</p>'); ?>
+			</div>
+			</div>
+			<!-- Text input-->
+			<div class="control-group">
+			<label class="control-label">Slug</label>
+			<div class="controls">
+			<input value="<?php echo set_value('slug'); ?>" class="form-control" type="text" name="slug" />
+			<?php echo form_error('slug','<p class="error">', '</p>'); ?>
+			</div>
+			</div>
+
+<div class="control-group">
+<label class="control-label"></label>
+	<div class="controls">
+		<button class="btn" type="submit">Crear</button>
+	</div>
+</div>
+
+
+
+</fieldset>
+
+<?php echo form_close(); ?>
+
+</div>
