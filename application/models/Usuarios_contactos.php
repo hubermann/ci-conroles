386046e1e24e945a2c9342ad0484f2662eea1894
Gabriel Hubermann <hub3r@MacBookAir.local>
2018-06-24 23:38:04 -0300

    removed profiler

--- application/models/Usuarios_contactos.php
+++ application/models/Usuarios_contactos.php
@@ -5,12 +5,11 @@ class Usuarios_contactos extends CI_Model{
 	public function __construct()
   {
 	   $this->load->database();
-     $this->output->enable_profiler(TRUE);
 	}
 
 
   public function get_contactos($user_id)
-  { 
+  {
     $this->db->select('contactos_usuarios.usuario_id,contactos_usuarios.contacto_id, contactos_usuarios.telefono, contactos_usuarios.email, usuarios.id, usuarios.nickname, usuarios.apellido');
 		$this->db->from('contactos_usuarios');
 		$this->db->join('usuarios', 'contactos_usuarios.contacto_id = usuarios.id', 'left');
